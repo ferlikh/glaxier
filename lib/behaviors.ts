bc8527fb45178db7964e2f8c2f179965a4b79ed8
Fred Erlikh <erlikhf@gmail.com>
2020-10-06 14:28:08 -0400

    feat(beahviors): autoMouse

--- lib/behaviors.ts
+++ lib/behaviors.ts
@@ -2,15 +2,16 @@ import { exposable } from './exposables';
 import { VRButton } from './externals';
 import { Scene } from './scenes';
 import { Settable } from './utils';
+import { Vector2 } from 'three';
 
 const defaultExpose = (object: Settable, value) => {
     object.set(value);
 }
 export const autoExpose = (name: string, object: Settable | Function, init?: Function | any) => {
-    const setter = object instanceof Function? 
-        value => object(value): 
+    const setter = object instanceof Function ?
+        value => object(value) :
         value => defaultExpose(object, value);
-    init = init && init instanceof Function && !(object instanceof Function)? init.bind(object): init;
+    init = init && init instanceof Function && !(object instanceof Function) ? init.bind(object) : init;
     exposable(window, name)(setter, init);
 }
 
@@ -19,16 +20,16 @@ const defaultResize = (scene: Scene) => {
     const camera = scene.camera as THREE.PerspectiveCamera,
         width = window.innerWidth, height = window.innerHeight;
 
-    if(composer) composer.setSize(width, height);
+    if (composer) composer.setSize(width, height);
     renderer.setSize(width, height);
-    
-    if(camera) {
+
+    if (camera) {
         camera.aspect = width / height;
         camera.updateProjectionMatrix();
     }
 }
 export const autoResize = (scene: Scene, customResize?: (aspect: number) => void) => {
-    const resizeFn = !customResize? 
+    const resizeFn = !customResize ?
         defaultResize.bind(null, scene) // default behavior
         : customResize.bind(scene); // custom callback
     window.addEventListener('resize', () => resizeFn(window.innerWidth / window.innerHeight), false);
@@ -42,15 +43,15 @@ export const autoVRButton = renderer => defaultVRButton(renderer);
 
 export const autoKeySwitch = (object, config, matchCase = false) => {
     const keys = Object.keys(config);
-    if(!matchCase) {
+    if (!matchCase) {
         keys.filter(k => k.length === 1).forEach(key => {
-            const upper = key.toUpperCase(), 
+            const upper = key.toUpperCase(),
                 lower = key.toLowerCase();
-            
-            if(!(lower in config)) {
+
+            if (!(lower in config)) {
                 config[lower] = config[upper];
             }
-            else if(!(upper in config)) {
+            else if (!(upper in config)) {
                 config[lower] = config[upper];
             }
 
@@ -58,8 +59,21 @@ export const autoKeySwitch = (object, config, matchCase = false) => {
     }
     window.addEventListener('keydown', event => {
         const values = config[event.key]
-        if(!values) return;
+        if (!values) return;
         Object.entries(values)
             .forEach(([prop, target]) => object[prop] = target);
+    });
+}
+
+export const autoMouse = scope => {
+    const mouse = new Vector2();
+    Object.defineProperty(scope, 'mouse', {
+        value: mouse,
+        enumerable: true
+    });
+    window.addEventListener('mousemove', event => {
+        event.preventDefault();
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
     });
 }
