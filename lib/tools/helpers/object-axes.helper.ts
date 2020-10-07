6844c68f32c9a77c09ecb5cc1fcffb4d1541f13b
Fred Erlikh <erlikhf@gmail.com>
2020-10-06 23:59:00 -0400

    feat(tools): suped up object-axes helper

--- lib/tools/helpers/object-axes.helper.ts
+++ lib/tools/helpers/object-axes.helper.ts
@@ -1,24 +1,63 @@
-import { AxesHelper, ArrowHelper, Vector3 } from 'three';
+import { AxesHelper, ArrowHelper, Raycaster, Vector3, Object3D } from 'three';
 
 export class ObjectAxesHelper extends AxesHelper {
-    public readonly arrowHelpers: ArrowHelper[];
+    intersects: any;
+    public readonly raycaster: Raycaster = new Raycaster;
+    public readonly arrowHelpers: { x: ArrowHelper, y: ArrowHelper, z: ArrowHelper };
+    get arrowHelpersList() {
+        return [this.arrowHelpers.x, this.arrowHelpers.y, this.arrowHelpers.z];
+    }
     constructor() {
         super();
         const
             origin = new Vector3(0, 0, 0),
             length = 1,
             headLength = 0.25,
-            headWidth = 0.1;
+            headWidth = 0.15;
 
-        this.arrowHelpers = [
-            new ArrowHelper(new Vector3(1, 0, 0), origin, length, 0xff0000, headLength, headWidth), // x
-            new ArrowHelper(new Vector3(0, 1, 0), origin, length, 0x00ff00, headLength, headWidth), // y
-            new ArrowHelper(new Vector3(0, 0, 1), origin, length, 0x0000ff, headLength, headWidth), // z
-        ]
+        this.arrowHelpers = {
+            x: new ArrowHelper(new Vector3(1, 0, 0), origin, length, 0xff0000, headLength, headWidth),
+            y: new ArrowHelper(new Vector3(0, 1, 0), origin, length, 0x00ff00, headLength, headWidth),
+            z: new ArrowHelper(new Vector3(0, 0, 1), origin, length, 0x0000ff, headLength, headWidth),
+        };
 
-        this.add(...this.arrowHelpers);
-
+        this.add(...this.arrowHelpersList);
         this.type = 'ObjectAxesHelper';
     }
 
+    private getAxisIntersected(arrowHelper: Object3D) {
+        switch (arrowHelper) {
+            case this.arrowHelpers.x:
+                return 'x';
+            case this.arrowHelpers.y:
+                return 'y';
+            case this.arrowHelpers.z:
+                return 'z';
+        }
+    }
+
+    updateIntersections(mouse, camera) {
+        this.raycaster.setFromCamera(mouse, camera);
+        const
+            intersects = this.raycaster.intersectObjects(this.arrowHelpersList.map(arrow => arrow.children[1])),
+            hit = intersects[0];
+
+        if (hit) {
+            const 
+                arrowHelper = hit.object.parent,
+                axis = this.getAxisIntersected(arrowHelper);
+                
+            this.intersects = {
+                axis,
+                x: axis === 'x',
+                y: axis === 'y',
+                z: axis === 'z',
+            };
+        }
+        else {
+            this.intersects = null;
+        }
+
+        return this.intersects;
+    }
 }
