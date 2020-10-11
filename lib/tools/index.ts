5596e44d93b52eed31b720db4c169d96fc5bfbba
Fred Erlikh <erlikhf@gmail.com>
2020-10-11 04:56:17 -0400

    feat(tools): vectors

--- lib/tools/index.ts
+++ lib/tools/index.ts
@@ -1,2 +1,3 @@
 export * from './helpers';
 export * from './tools';
+export * from './vectors';
---
+++ lib/tools/vectors.ts
@@ -0,0 +1,27 @@
+/*
+*    A namespace for vector functions
+*/
+
+import { Vector2, Vector3 } from 'three';
+
+export class Vectors {
+    static distance(u: Vector2, v: Vector2, direction?: Vector2) {
+        const { x: x1, y: y1 } = u;
+        const { x: x2, y: y2 } = v;
+        const dX = x2 - x1, dY = y2 - y1;
+        return Vectors.sign(direction, new Vector2(dX, dY)) * Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    static sign(v, d) {
+        if(!v || !d) return 1;
+        return v.dot(d) >= 0? +1: -1;
+    }
+
+    static unit(axis: 'x' | 'y' | 'z') {
+        switch (axis) {
+            case 'x': return new Vector3(1, 0, 0);
+            case 'y': return new Vector3(0, 1, 0);
+            case 'z': return new Vector3(0, 0, 1);
+        }
+    }
+}
